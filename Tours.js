23bb19da31e3e153bc2893266519ce9ce0ae86ef
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-08-13 02:36:40 +0300

    Add files via upload

---
+++ Tours.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Tour from "./Tour";
+const Tours = ({ tours ,removeTour}) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline"></div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour}  removeTour={removeTour}></Tour>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
